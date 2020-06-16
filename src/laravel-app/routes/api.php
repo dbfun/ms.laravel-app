89a5d5b016317aa27400de0981488691983efb92
pushorigin <info@pushorigin.ru>
2020-06-16 17:20:54 +0300

    добавил роут /healthcheck

--- src/laravel-app/routes/api.php
+++ src/laravel-app/routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::get('/healthcheck', function (Request $request) {
+    return response()->json([
+        'result' => 'ok'
+    ]);
 });
